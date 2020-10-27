17c979ab0f986ac3211893fa0fcf186b3faead37
Gabriel Barros <barros.fontes@unesp.br>
2020-10-27 14:15:28 -0300

    começando o layout de input post

---
+++ src/components/InputPost.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function InputPost() {
+    
+
+    return (
+        <>
+            <div>
+                <img src="https://pm1.narvii.com/6055/c57c6dd83178676ae35485d77bd40ffb5b34ccf6_hq.jpg"/>
+                <div>
+                    <h1>O que você tem para favoritar hoje?</h1>
+                    <input placeholder="http://..."/>
+                    <textarea rows="5"/>
+                </div>
+            </div>
+        </>
+    )
+}
