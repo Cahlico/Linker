a4989786e64dd9134e3e9562c4d339cd302ae4b0
Gabriel Barros <barros.fontes@unesp.br>
2020-10-27 16:50:17 -0300

    enviando o post pro servidor

--- src/components/InputPost.js
+++ src/components/InputPost.js
@@ -1,25 +1,32 @@
 import React,{useState} from 'react'
+import axios from 'axios';
 
 import {InputContainer} from '../styles/timeline';
 
 export default function InputPost(props) {
-    const {avatar} = props;
+    const {token, user} = props.userData;
     const [link,setLink] = useState('');
-    const [description,setDescription] = useState('');
-
+    const [text,setText] = useState('');
+    const [clicked,setClicked] = useState(false);
 
     
     function publish() {
-        console.log(description,link);
-        if(description===''||link==='') {
+        
+        if(text===''||link==='') {
             alert("Preencha todos os campos, por favor");
         }
+
+        if(clicked) return;
+
+        const request = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v1/linkr/posts",{ link , text },{headers: {"User-Token": token}});
+
+        request.then(response => console.log(response)).catch(response=>console.log(response));
     }
 
     return (
         <>
             <InputContainer>
-                <img src={avatar}/>
+                <img src={user.avatar}/>
                 <div>
                     <h1>O que você tem para favoritar hoje?</h1>
                     <input 
@@ -30,8 +37,8 @@ export default function InputPost(props) {
                     <textarea 
                     rows="5" 
                     placeholder="Comente sobre o link !"
-                    onChange={e => setDescription(e.target.value)}
-                    value={description}
+                    onChange={e => setText(e.target.value)}
+                    value={text}
                     />
                     <div><button onClick={publish}>Publicar</button></div>
                 </div>
