432f2e63e2f36c5099d7f2a08346e16da2aa79f6
Gabriel Barros <barros.fontes@unesp.br>
2020-10-27 19:23:53 -0300

    passando os atributos pro header e inputPost

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -4,19 +4,25 @@ import UserContext from '../contexts/UserContext';
 
 import Header from '../components/Header';
 import InputPost from '../components/InputPost';
+import TrendingTopics from '../components/TrendingTopics';
 
 import { MainContainer } from '../styles/timeline'
 
 export default function Timeline() {
-    const data = useContext(UserContext);
-    const { userInfo } = data;
-    console.log(userInfo);
+    const { userInfo } = useContext(UserContext);
+    const userData = userInfo.data
+    if(userData === undefined) {
+        window.location = "http://localhost:9000";
+    }
+    const { avatar } = userData.user;
+    console.log(userData);
     return (
         <>
-            <Header/>
+            <Header avatar = {avatar}/>
             <MainContainer>
                 <h1>timeline</h1>
-                <InputPost/>
+                <InputPost userData = {userData} />
+                {/* <TrendingTopics/> */}
             </MainContainer>
         </>
     )
