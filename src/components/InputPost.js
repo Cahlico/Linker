4de4461c8d32b69ade5f11d47736769d76a238ca
Gabriel Barros <barros.fontes@unesp.br>
2020-10-27 16:24:52 -0300

    valida se os camps estao vazios

--- src/components/InputPost.js
+++ src/components/InputPost.js
@@ -1,19 +1,39 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 import {InputContainer} from '../styles/timeline';
 
-export default function InputPost() {
+export default function InputPost(props) {
+    const {avatar} = props;
+    const [link,setLink] = useState('');
+    const [description,setDescription] = useState('');
+
+
     
+    function publish() {
+        console.log(description,link);
+        if(description===''||link==='') {
+            alert("Preencha todos os campos, por favor");
+        }
+    }
 
     return (
         <>
             <InputContainer>
-                <img src="https://pm1.narvii.com/6055/c57c6dd83178676ae35485d77bd40ffb5b34ccf6_hq.jpg"/>
+                <img src={avatar}/>
                 <div>
                     <h1>O que você tem para favoritar hoje?</h1>
-                    <input placeholder="http://..."/>
-                    <textarea rows="5" placeholder="Comente sobre o link !"/>
-                    <div><button>Publicar</button></div>
+                    <input 
+                    placeholder="http://..."
+                    onChange={e => setLink(e.target.value)}
+                    value={link}
+                    />
+                    <textarea 
+                    rows="5" 
+                    placeholder="Comente sobre o link !"
+                    onChange={e => setDescription(e.target.value)}
+                    value={description}
+                    />
+                    <div><button onClick={publish}>Publicar</button></div>
                 </div>
             </InputContainer>
         </>
